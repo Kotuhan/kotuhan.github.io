af97a2ba98332b38edef35098b5780cc25bd285f
Bohdan Denysiuk <kotuchi0203@gmail.com>
2019-04-07 19:33:52 +0300

    Implement rates app

--- myScript.js
+++ myScript.js
@@ -1,17 +1,48 @@
-var content = document.getElementById("content");
+function createCell(text, rate) {
+  return `
+    <span class="text">
+      ${text}
+    </span>
 
-function addChildren() {
-  for (let index = 0; index < 16; index++) {
-    let newImage = document.createElement("img");
-
-    let src = "http://picsum.photos/100/100/?" + index;
-
-    newImage.setAttribute("src", src);
-
-    newImage.setAttribute("class", "image");
-
-    content.appendChild(newImage);
-  }
+    <span class="rate">
+      ${rate}
+    </span>
+  `;
 }
 
-addChildren();
+const appendToTable = dataItem => {
+  const container = document.getElementById("container");
+  const div = document.createElement("div");
+
+  div.setAttribute("class", "cell");
+  div.innerHTML = createCell(dataItem.txt, dataItem.rate);
+
+  container.appendChild(div);
+};
+
+function showData(data) {
+  console.log("data?", data);
+
+  data.forEach(appendToTable);
+}
+
+// promise based
+// function fetchData() {
+//   fetch("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json")
+//     .then(response => {
+//       return response.json();
+//     })
+//     .then(json => {
+//       showData(json);
+//     });
+// }
+
+//async/await
+const fetchData = async () => {
+  const response = await fetch(
+    "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json"
+  );
+  const data = await response.json();
+
+  showData(data);
+};
